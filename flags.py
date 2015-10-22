3f8eb11167e7aad17a33e29f85e0d0a5719f6171
Nar Chhantyal <nkchhantyal@gmail.com>
2015-10-22 23:44:19 +0200

    flag mozilla related packages. Refer to #22 for more details.

--- flags.py
+++ flags.py
@@ -1,4 +1,16 @@
 
 FLAGS = {
     "rackspace-novaclient":"https://github.com/rackerlabs/rackspace-novaclient",
+    "mozrunner":"https://pypi.python.org/pypi/mozrunner",
+    "moznetwork":"https://pypi.python.org/pypi/moznetwork",
+    "mozdevice": "https://pypi.python.org/pypi/mozdevice",
+    "mozprofile": "https://pypi.python.org/pypi/mozprofile",
+    "mozprocess": "https://pypi.python.org/pypi/mozprocess",
+    "mozfile": "https://pypi.python.org/pypi/mozfile",
+    "mozinfo": "https://pypi.python.org/pypi/mozinfo",
+    "mozlog": "https://pypi.python.org/pypi/mozlog",
+    "mozcrash": "https://pypi.python.org/pypi/mozcrash",
+    "mozhttpd": "https://pypi.python.org/pypi/mozhttpd",
+    "moztest": "https://pypi.python.org/pypi/moztest",
+    "mozversion": "https://pypi.python.org/pypi/mozversion",
 }
