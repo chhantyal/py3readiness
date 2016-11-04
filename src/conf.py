5bc727b16de313e63a07c6862a8f9b4c6f5dde87
Nar Chhantyal <nkchhantyal@gmail.com>
2016-11-04 22:34:33 +0100

    It's CacheControl not Cache Control

--- src/conf.py
+++ src/conf.py
@@ -5,7 +5,7 @@ s3_client = boto3.client('s3')
 bucket = 'uhura.de.public'
 
 metadata = {
-    "Cache Control": "max-age=21600, public"  # 6 hours
+    "CacheControl": "max-age=21600, public"  # 6 hours
 }
 
 BASE_URL = 'https://pypi.python.org/pypi'
